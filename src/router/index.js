6e340a4797841f5027aaa94f0d2d64c678fa55da
Chima Ejiofor <mobiblunt@gmail.com>
2023-05-12 18:18:59 +0100

    removed routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-  {
-    path: '/',
-    name: 'dashboard',
-    component: () => import('@/views/Dashboard.vue'),
-    children: [
-      {
-        path: '',
-        component: () => import('@/views/dashboard/Home.vue'),
-      },
-      {
-        path: 'users',
-        name: 'user-list',
-        component: () => import('@/views/dashboard/users/UserList.vue'),
-      },
-      {
-        path: '/card',
-        name: 'card',
-        component: () => import('@/views/Card.vue'),
-      },
-    ],
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: () => import('@/views/Login.vue'),
-  },
+  
+  
 
   {
       path: '/user/:userId',
